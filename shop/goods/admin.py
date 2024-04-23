0c7a41a93ed7a93592e106748c920bfabb4a2ede
EldarK-NS <eldark.ns@gmail.com>
2024-04-23 11:19:34 +0500

    added poduct in admin

--- shop/goods/admin.py
+++ shop/goods/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from goods.models import Categories
+from goods.models import Categories, Products
 
 admin.site.register(Categories)
+admin.site.register(Products)
