1a5e7f277fc3151a7bed27cec5f557bc7c2bf100
EldarK-NS <eldark.ns@gmail.com>
2024-04-25 14:50:47 +0500

    fix

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 # Create your views here.
 # такая функция в файле views называется представлением или контроллером
 def index(request):
-    context = {"title": "Home - Главная", "content": "Магазин мебели HOME"}
+    categories = Categories.objects.all()
+    context = {
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+        "categories": categories,
+    }
     return render(request, "main/index.html", context)
 
 
 def about(request):
+
     context = {
         "title": "Home - - О нас",
         "content": "О нас",
