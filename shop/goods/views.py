8d8a83200bbf15cd7ba878c89d051bc34b5575c5
EldarK-NS <eldark.ns@gmail.com>
2024-04-30 08:10:26 +0500

    added pagination

--- shop/goods/views.py
+++ shop/goods/views.py
@@ -1,22 +1,24 @@
+from django.core.paginator import Paginator
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from goods.models import Products
 
 # Create your views here.
 #! Controllers
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug == "all":
         goods = Products.objects.all()
     else:
-        try:
-            goods = Products.objects.filter(category__slug=category_slug)
-        except Products.DoesNotExist:
-            raise Http404("No MyModel matches the given query.")
-        # goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
-
-    context = {"title": "Home - Каталог", "goods": goods}
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    paginator = Paginator(goods, 2)
+    current_page = paginator.page(page)
+    context = {
+        "title": "Home - Каталог",
+        "goods": current_page,
+        "slug_url": category_slug,
+    }
 
     return render(request, "goods/catalog.html", context)
 
