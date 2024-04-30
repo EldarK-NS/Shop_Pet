5a6ea8f770790fa74fb2c9e4ade7a236b6560d31
EldarK-NS <eldark.ns@gmail.com>
2024-04-30 11:50:38 +0500

    fix pagination

--- shop/goods/views.py
+++ shop/goods/views.py
@@ -12,7 +12,7 @@ def catalog(request, category_slug, page=1):
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-    paginator = Paginator(goods, 2)
+    paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
     context = {
         "title": "Home - Каталог",
